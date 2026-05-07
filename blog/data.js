7ecddd8b8c3eb35dbabd8717e353ac1137ef4ccb
evandaru <fznhsym@gmail.com>
2026-05-07 23:11:18 +0700

    update

---
+++ blog/data.js
@@ -0,0 +1,98 @@
+// Data Website
+const websites = [
+  {
+    name: "Svg Repo",
+    url: "https://www.svgrepo.com/",
+    description: "buat cari icon brand dan lain lain",
+    icon: "fa-globe",
+    tags: ["design","icon"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "blog gw",
+    url: "https://blog.fauzanhasyim.my.id/",
+    description: "buat cari icon brand dan lain lain",
+    icon: "fa-globe",
+    tags: ["library"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "Iconify",
+    url: "https://iconify.design/",
+    description: "buat cari icon",
+    icon: "fa-globe",
+    tags: ["design","icon"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "tools design",
+    url: "https://www.toools.design/",
+    description: "banyak tools disini",
+    icon: "fa-globe",
+    tags: ["library"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "uiverse",
+    url: "https://uiverse.io/",
+    description: "banyak referensi code",
+    icon: "fa-globe",
+    tags: ["code"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "DownGit",
+    url: "https://downgit.github.io/#/home",
+    description: "buat download repo github",
+    icon: "fa-globe",
+    tags: ["downloader"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "img to vector",
+    url: "https://svgco.de/",
+    icon: "fa-globe",
+    tags: ["design"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "Pdf kambing",
+    url: "https://tools.pdf24.org/en/",
+    icon: "fa-globe",
+    tags: ["pdf"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "tinywow",
+    url: "https://tinywow.com/",
+    icon: "fa-globe",
+    tags: ["all in one"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "VibeUI",
+    url: "https://vibeui.online/",
+    description: "ini buat vibe coding",
+    icon: "fa-globe",
+    tags: ["code"],
+    color: "bg-yellow-400",
+  },
+  {
+    name: "Temu Karir",
+    url: "https://temukarir.com/",
+    description: "ini buat vibe coding",
+    icon: "fa-globe",
+    tags: ["karir"],
+    color: "bg-yellow-400",
+  },
+];
+
+// Data Prompt AI
+const prompts = [
+  {
+    title: "KOMIK GENERATOR",
+    text: "Create a high-quality black-and-white comic page in portrait orientation with exactly 12 panels, featuring cinematic composition, varied shot angles, detailed ink linework and cross-hatching, expressive characters, clear visual storytelling (setup–conflict–climax–resolution), immersive dialogue in speech bubbles, and a cohesive professional manga style with clean panel layout and strong contrast.",
+    tags: ["Img"],
+    color: "bg-yellow-300",
+  },
+];
